fa5587c89c59013d7106f24f15a75994c8039c7f
Klas Björkqvist <klas.bjorkqvist@gmail.com>
2014-04-17 00:09:11 +0200

    Add signal handler for SIGCHLD, to not get defunct processes.

--- source/mwm.d
+++ source/mwm.d
@@ -8,9 +8,22 @@ import mwm.xrunner;
 import mwm.wm;
 import mwm.common;
 
+import core.sys.posix.signal;
+import core.sys.posix.sys.wait;
+
+extern(C) void sigchld(int _) nothrow @system {
+  try {
+    while (0 < waitpid(-1, null, WNOHANG)) { }
+  } catch { }
+}
+
 
 int main()
 {
+  if (signal(SIGCHLD, &sigchld) == SIG_ERR) {
+    wr("unable to install sigchld handler");
+  }
+
   auto wmthread = new Thread(&mwm.wm.run);
   auto xthread = new Thread(&mwm.xrunner.run);
 
